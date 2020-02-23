ea6b7bc4454b7e2357935dedf8b2b4acfb86747f
Corey Megown <corey.megown@gmail.com>
2020-02-23 00:00:03 -0600

    updating script

--- tabs.js
+++ tabs.js
@@ -1,35 +1,13 @@
 export class Tabs {
   constructor() {
-    this.tabs = document.querySelectorAll('.c-tab');
-    this.tablists = document.querySelectorAll('.c-tablist');
-    this.tabpanels = document.querySelectorAll('.c-tabpanel');
+    //
   }
 
   create() {
-    this.tabs.forEach(tab => {
-      tab.setAttribute('role', 'tab');
-    });
-
-    this.tablists.forEach(tablist => {
-      tablist.setAttribute('role', 'tablist');
-    });
-
-    this.tabpanels.forEach(tabpanel => {
-      tabpanel.setAttribute('role', 'tabpanel');
-    });
+    //
   }
 
   destroy() {
-    this.tabs.forEach(tab => {
-      tab.removeAttribute('role');
-    });
-
-    this.tablists.forEach(tablist => {
-      tablist.removeAttribute('role');
-    });
-
-    this.tabpanels.forEach(tabpanel => {
-      tabpanel.removeAttribute('role');
-    });
+    //
   }
 }
