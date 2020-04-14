2d85f60eaac29ba26b6c3d1f428dba0aa9d74a77
Corey Megown <corey.megown@gmail.com>
2020-04-14 17:46:22 -0500

    updating tabs component

--- tabs.js
+++ tabs.js
@@ -1,8 +1,19 @@
 export class Tabs {
   constructor() {
-    this.tabs = document.querySelectorAll('.c-tab');
-    this.tablists = document.querySelectorAll('.c-tablist');
-    this.tabpanels = document.querySelectorAll('.c-tabpanel');
+    this.tabs = document.querySelectorAll('.tab');
+    this.tablists = document.querySelectorAll('.tablist');
+    this.tabpanels = document.querySelectorAll('.tabpanel');
+    this.containers = document.querySelectorAll('.container');
+  }
+
+  ///
+
+  _closest(element, selector) {
+    while (element) {
+      if (element.matches(selector)) break;
+      element = element.parentElement;
+    }
+    return element;
   }
 
   ///
@@ -11,6 +22,7 @@ export class Tabs {
     this.tabs.forEach(tab => {
       tab.setAttribute('role', 'tab');
       tab.setAttribute('aria-selected', 'false');
+      tab.parentElement.setAttribute('role', 'presentation');
     });
 
     this.tablists.forEach(tablist => {
@@ -19,33 +31,52 @@ export class Tabs {
 
     this.tabpanels.forEach(tabpanel => {
       tabpanel.setAttribute('role', 'tabpanel');
-      tabpanel.setAttribute('tabindex', '-1');
+      tabpanel.hidden = true;
     });
   }
 
-  _removeAria() {
+  _bindTabEvents() {
     this.tabs.forEach(tab => {
-      tab.removeAttribute('role');
-      tab.removeAttribute('aria-selected');
+      tab.addEventListener('click', this._eventTabClick.bind(this));
+    });
+  }
+
+  _eventTabClick(e) {
+    e.preventDefault();
+    this._showTab(e.target);
+  }
+
+  _showTab(target) {
+    let i = 0;
+    let container = this._closest(target, '.container');
+    let tabs = container.querySelectorAll('.tab');
+    let tabpanels = container.querySelectorAll('.tabpanel');
+
+    tabs.forEach((tab, index) => {
+      if (tab === target) i = index;
+      tab.setAttribute('aria-selected', 'false');
     });
 
-    this.tablists.forEach(tablist => {
-      tablist.removeAttribute('role');
-    });
+    tabpanels.forEach(tabpanel => {
+      tabpanel.hidden = true;
+    })
 
-    this.tabpanels.forEach(tabpanel => {
-      tabpanel.removeAttribute('role');
-      tabpanel.removeAttribute('tabindex');
-    });
+    target.setAttribute('aria-selected', 'true');
+    tabpanels[i].hidden = false;
   }
 
   ///
 
   create() {
     this._addAria();
+    this._bindTabEvents();
+
+    this.containers.forEach(container => {
+      this._showTab(container.querySelectorAll('.tab')[0]);
+    });
   }
 
   destroy() {
-    this._removeAria();
+    //
   }
 }
